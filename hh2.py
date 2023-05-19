ecee319254a720bb1d912c1513de2f1d86352193
yasya110865 <evn04@mail.ru>
2023-05-19 11:19:02 +0600

    html_css

--- hh2.py
+++ hh2.py
@@ -5,7 +5,7 @@ from collections import Counter
 import json
 
 url = 'https://api.hh.ru/vacancies'
-vacance = 'Data scientist'
+vacance = 'python'
 params = {
     'text': vacance,'area': 1,
     # есть страницы т.к. данных много
@@ -44,43 +44,48 @@ for i in range(len(it)):
 mean_salaryfrom = round(sum(s_from)/len(s_from))
 mean_salaryto = round(sum(s_to)/len(s_to))
 
+# это будет список навыков (в виде словарей name: навык)
+
 collect_skills = []
+#тут индекс чтобы не замучать HH
 for i in range(len(it[:10])):
 
     vac_url = it[i]['url']
     vac = requests.get(vac_url).json()
     time.sleep(1)
-    print(vac)
+    # print(vac)
     nonempty_skills = 0
-
+# если не пустое значение у очередной вакансии
     if vac['key_skills']:
         skills_dict = vac['key_skills']
-        print(skills_dict)
+        # print(skills_dict)
         nonempty_skills += 1
+        # то добавляем в список
         collect_skills.extend(skills_dict)
 
-print(collect_skills)
-print(len(collect_skills))
+# print(collect_skills)
+# print(len(collect_skills))
 
-
+# делаем список из значений
 skills_list = []
 for i in range(len(collect_skills)):
     skill = list(collect_skills[i].values())
     skills_list.extend(skill)
-print(skills_list)
+# print(skills_list)
+# считакм количесьво по каждому навыку
 skills_result = Counter(skills_list)
-print(dict(skills_result))
+# и сортируем
 final_skills = dict(sorted(skills_result.items(), key=lambda x: x[1], reverse=True))
-print(final_skills)
-print(len(final_skills))
-print(sum(list(final_skills.values())))
-
+# print(final_skills)
+# print(len(final_skills))
+# print(sum(list(final_skills.values())))
+# теперь проценты по количеству каждого навыка
 persents = []
 for i in range(len(final_skills)):
     pers = round(list(final_skills.values())[i] * 100/sum(final_skills.values()))
     persents.append(pers)
-print(persents)
-print(sum(persents))
+# print(persents)
+# print(sum(persents))
 
 
 #cписок скиллов, в него добавляем по каждому требуемому навыку название, количество и процент
@@ -102,9 +107,9 @@ final = {
 }
 
 
-with open('hh.json', 'w') as f:
+with open('hh.json', 'w', encoding='utf-8') as f:
     json.dump(final, f)
 
-with open('hh.json', 'r') as f:
+with open('hh.json', 'r', encoding='utf-8') as f:
     text = json.load(f)
-    print(json.dumps(text, indent=2, sort_keys=True))
+    print(json.dumps(text, indent=2, ensure_ascii=False, sort_keys=True))
